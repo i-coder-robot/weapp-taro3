3ee94c7a5c40810918da4071514bf2855babdedc
Monster <coolpest@gmail.com>
2020-12-08 21:27:45 +0800

    init

---
+++ src/pages/me/index.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Me!</Text>
+        <AtButton type='secondary' circle>来</AtButton>
+      </View>
+    )
+  }
+}
