35047191bda917444f029ad32fd796ac3f796865
Monster <coolpest@gmail.com>
2020-12-08 21:38:09 +0800

    constants

---
+++ src/constants/api.js
@@ -0,0 +1,7 @@
+export const host = 'http://localhost:9090/v1/dp'
+
+//home
+export const API_HOME_NAV = `${host}/nav`
+export const API_HOME_SUB_NAV = `${host}/subNav`
+export const API_HOME_TEAM = `${host}/team`
+export const API_HOME_RUSH = `${host}/rush`
